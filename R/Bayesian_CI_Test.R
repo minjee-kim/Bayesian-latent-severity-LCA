55b2873dfbd1c1a3a2a3728c4c444e1523e0b4de
Minjee Kim <minjeekim@MacBook-Pro-3.local>
2025-10-28 09:41:02 -0700

    cleaning up the folders

--- R/Bayesian_CI_Test.R
+++ R/Bayesian_CI_Test.R
@@ -5,7 +5,7 @@ Bayesian_CI_Test <- function(test1, test2, D_posterior){
   S <- nrow(D_posterior)
   N <- length(test1)
   
-  chisq <- matrix(NA_real_, nrow = S, ncol = 2, dimnames = list(NULL, c("d=0","d=1")))
+  chisq <- matrix(NA, nrow = S, ncol = 2, dimnames = list(NULL, c("d=0","d=1")))
 
   for (s in 1:S) {
     Ds <- D_posterior[s, ]
@@ -13,10 +13,10 @@ Bayesian_CI_Test <- function(test1, test2, D_posterior){
       idx <- which(Ds == d)
       if (length(idx) < 2) next
       
-      t1 <- test1[idx] + 1L
-      t2 <- test2[idx] + 1L
-      tab <- matrix(0L, 2, 2)
-      for (k in seq_along(t1)) tab[t1[k], t2[k]] <- tab[t1[k], t2[k]] + 1L
+      t1 <- test1[idx] + 1
+      t2 <- test2[idx] + 1
+      tab <- matrix(0, 2, 2)
+      for (k in seq_along(t1)) tab[t1[k], t2[k]] <- tab[t1[k], t2[k]] + 1
       
       n  <- sum(tab)
       rs <- rowSums(tab)

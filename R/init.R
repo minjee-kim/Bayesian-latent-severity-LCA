b0432ffffaea4bbd3a65f7648ae219cd6995782d
Minjee Kim <minjeekim@MacBook-Pro-3.local>
2025-10-28 13:59:26 -0700

    created an init file to call all relevant functions

---
+++ R/init.R
@@ -0,0 +1,10 @@
+
+
+source("Gamma_LCA_severity.R")
+source("CI_LCA_probit.R")
+source("build_priors_from_ranges.R")
+source("Bayesian_LCA_severity.R")
+source("plot_prior_vs_posterior.R")
+
+source("bayes_2LCR.R")
+source("plot_prior_vs_posterior_2LCR.R")
